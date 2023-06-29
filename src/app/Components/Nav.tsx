56aec3f48d937df97378cc3f36a042b57ec8543c
Pablo Escobar <esc20936@uvg.edu.gt>
2023-06-29 17:10:28 -0600

    format documents

--- src/app/Components/Nav.tsx
+++ src/app/Components/Nav.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { motion, AnimatePresence } from 'framer-motion';
+import React, { useState } from "react";
+import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
 // import { Link } from 'react-router-dom'; // If you're using React Router
 
 const Navbar = () => {
@@ -32,13 +32,13 @@ const Navbar = () => {
                 href="/about"
                 className="text-gray-900 hover:text-gray-400 px-3 py-2 rounded-md text-sm font-medium"
               >
-               Automatización
+                Automatización
               </Link>
               <Link
                 href="/about"
                 className="text-gray-900 hover:text-gray-400 px-3 py-2 rounded-md text-sm font-medium"
               >
-               Emotron
+                Emotron
               </Link>
               <Link
                 href="/contact"
@@ -56,99 +56,100 @@ const Navbar = () => {
             </div>
           </div>
           <div className="block md:hidden">
-          <motion.button
-      onClick={toggleNavbar}
-      className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 focus:outline-none"
-      aria-expanded={isOpen}
-      whileTap={{ scale: 0.9 }} // Example of tap animation, adjust as needed
-    >
-      <span className="sr-only">Open main menu</span>
-      {isOpen ? (
-        <motion.svg
-          className="block h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-          initial={{ opacity: 0 }} // Initial state before animation
-          animate={{ opacity: 1 }} // Animation when the button is rendered
-          transition={{ duration: 0.3 }} // Transition duration
-        >
-          <motion.path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </motion.svg>
-      ) : (
-        <motion.svg
-          className="block h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-          initial={{ opacity: 0 }} // Initial state before animation
-          animate={{ opacity: 1 }} // Animation when the button is rendered
-          transition={{ duration: 0.3 }} // Transition duration
-        >
-          <motion.path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </motion.svg>
-      )}
-    </motion.button>
+            <motion.button
+              onClick={toggleNavbar}
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 focus:outline-none"
+              aria-expanded={isOpen}
+              whileTap={{ scale: 0.9 }} // Example of tap animation, adjust as needed
+            >
+              <span className="sr-only">Open main menu</span>
+              {isOpen ? (
+                <motion.svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  initial={{ opacity: 0 }} // Initial state before animation
+                  animate={{ opacity: 1 }} // Animation when the button is rendered
+                  transition={{ duration: 0.3 }} // Transition duration
+                >
+                  <motion.path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </motion.svg>
+              ) : (
+                <motion.svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  initial={{ opacity: 0 }} // Initial state before animation
+                  animate={{ opacity: 1 }} // Animation when the button is rendered
+                  transition={{ duration: 0.3 }} // Transition duration
+                >
+                  <motion.path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </motion.svg>
+              )}
+            </motion.button>
           </div>
         </div>
       </div>
       <AnimatePresence>
-      {isOpen && (
-        <motion.div 
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 20 }}
-        className="md:hidden absolute w-full z-10 rounded-sm bg-white bg-opacity-10 backdrop-blur-lg rounded drop-shadow-lg">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <Link
-              href="/"
-              className="text-gray-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Inicio
-            </Link>
-            <Link
-              href="/about"
-              className="text-gray-900 hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Automatización
-            </Link>
-            <Link
-              href="/contact"
-              className="text-gray-900 hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Emotron
-            </Link>
-            <Link
-              href="/contact"
-              className="text-gray-900 hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
-            >
-              CCTV-Cámaras
-            </Link>
-            <Link
-              href="/contact"
-              className="text-gray-900 hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Asesorias
-            </Link>
-            {/* Add more navigation links as needed */}
-          </div>
-        </motion.div>
-      )}
-        </AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            className="md:hidden absolute w-full z-10 rounded-sm bg-white bg-opacity-10 backdrop-blur-lg rounded drop-shadow-lg"
+          >
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <Link
+                href="/"
+                className="text-gray-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Inicio
+              </Link>
+              <Link
+                href="/about"
+                className="text-gray-900 hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Automatización
+              </Link>
+              <Link
+                href="/contact"
+                className="text-gray-900 hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Emotron
+              </Link>
+              <Link
+                href="/contact"
+                className="text-gray-900 hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
+              >
+                CCTV-Cámaras
+              </Link>
+              <Link
+                href="/contact"
+                className="text-gray-900 hover:text-gray-400 block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Asesorias
+              </Link>
+              {/* Add more navigation links as needed */}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,25 +18,24 @@ export default function Home() {
             initial="hidden"
             animate="visible"
             variants={containerVariants}
-            className="" 
+            className=""
           >
             <h1 className="text-6xl md:text-8xl font-bold  colorText">
               E3 Solutions
             </h1>
             <p className="mt-3 text-2xl  text-blue-900  font-semibold">
-            Potenciando tu éxito con innovación
+              Potenciando tu éxito con innovación
             </p>
 
             <div className="flex flex-row items-center justify-center mt-10 space-x-4">
-              
               <motion.button
                 whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }} 
-                className="bg-blue-600 text-white px-6 py-2 rounded-md text-sm"
+                whileTap={{ scale: 0.9 }}
+                className="bg-blue-500 text-white px-6 py-2 rounded-md text-sm"
               >
-                <a href="#contact" 
-                className="font-medium"
-                >Contáctanos</a>
+                <a href="#contact" className="font-medium">
+                  Contáctanos
+                </a>
               </motion.button>
 
               <motion.button
@@ -44,12 +43,10 @@ export default function Home() {
                 whileTap={{ scale: 0.9 }}
                 className=" text-black border-solid border-2 border-gray-500  px-6 py-2 rounded-md text-sm"
               >
-                <a href="#contact"
-                className="font-medium"
-                >Quienes somos</a>
+                <a href="#contact" className="font-medium">
+                  Quienes somos
+                </a>
               </motion.button>
-              
-
             </div>
           </motion.div>
         </div>
